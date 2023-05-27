2e9c39524ab3639c6e25a88c8b70b0438f8deda3
Alviere <againes0501@gmail.com>
2023-05-26 23:46:26 -0500

    Day 12 part 2 successful

--- day12/day12.cpp
+++ day12/day12.cpp
@@ -10,6 +10,22 @@ std::vector<std::vector<int>> search_map;
 std::vector<std::array<int,3>> search_threads;
 int length_found;
 
+void read_start() {
+    int x, y;
+    for (int y = 0; y < height_map.size(); y++) {
+        if ((x = height_map[y].find('E')) == (std::string::npos))
+            continue;
+        
+        search_threads.clear();
+        search_threads.push_back({x,y});
+    }
+}
+
+void cleanse_search() {
+    search_map = std::vector<std::vector<int>>(height_map.size(), std::vector<int>(height_map[0].length(), -1));
+    read_start();
+}
+
 int read_map() {
     ifstream input("input.txt");
     if (!input.is_open()) {
@@ -22,19 +38,8 @@ int read_map() {
     while (getline(input, line))
         height_map.push_back(line);
     
-    search_map = std::vector<std::vector<int>>(height_map.size(), std::vector<int>(height_map[0].length(), -1));
+    cleanse_search();
     return 0;
-}
-
-void read_start() {
-    int x, y;
-    for (int y = 0; y < height_map.size(); y++) {
-        if ((x = height_map[y].find('S')) == (std::string::npos))
-            continue;
-        
-        search_threads.clear();
-        search_threads.push_back({x,y});
-    }
 }
 
 bool valid_move(std::array<int,3> current_coords, std::array<int,3> next_coords) {
@@ -46,16 +51,16 @@ bool valid_move(std::array<int,3> current_coords, std::array<int,3> next_coords)
     
     char current_pos = height_map[current_coords[1]][current_coords[0]];
     char next_pos = height_map[next_coords[1]][next_coords[0]];
-    current_pos = (current_pos == 'S') ? 'a' : current_pos;
-    next_pos = (next_pos == 'E') ? 'z' : next_pos;
+    current_pos = (current_pos == 'E') ? 'z' : current_pos;
+    next_pos = (next_pos == 'S') ? 'a' : next_pos;
 
-    if (next_pos > current_pos + 1)
+    if (next_pos < current_pos - 1)
         return false;
 
     return true;
 }
 
-void propagate_first_search() {
+void propagate_first_search(char target) {
     std::array<int,3> coords = search_threads.front();
     search_threads.erase(search_threads.begin());
     
@@ -66,12 +71,11 @@ void propagate_first_search() {
 
     std::array<int,2> possible_moves[] = { {0,1}, {0,-1}, {1,0}, {-1,0} };
     for (std::array<int,2> offset : possible_moves) {
-
         std::array<int,3> offset_coords = { coords[0] + offset[0] , coords[1] + offset[1] , coords[2] + 1};
         if (!valid_move(coords, offset_coords))
             continue;
         
-        if (height_map[offset_coords[1]][offset_coords[0]] == 'E') {
+        if (height_map[offset_coords[1]][offset_coords[0]] == target) {
             length_found = offset_coords[2];
             return;
         }
@@ -83,15 +87,19 @@ void propagate_first_search() {
 
 int main() {
     if (read_map() == -1) return 0;
-    read_start();
 
     //for (std::string x : height_map)
     //    std::cout << x << std::endl;
     //std::cout << search_threads[0][0] << "," << search_threads[0][1] << std::endl;
     
     length_found = 0;
-    while (length_found == 0 && search_threads.size() > 0) propagate_first_search();
-
+    while (length_found == 0 && search_threads.size() > 0) propagate_first_search('S');
     std::cout << "Part 1: " << length_found << std::endl;
+
+    cleanse_search();
+    length_found = 0;
+    while (length_found == 0 && search_threads.size() > 0) propagate_first_search('a');
+    std::cout << "Part 2: " << length_found << std::endl;
+
     return 0;
 }
