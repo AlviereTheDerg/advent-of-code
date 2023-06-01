c1c5743e00470ef495dc3d122b9ee0ddd5732d93
Alviere <againes0501@gmail.com>
2023-06-01 05:43:32 -0500

    Day 16 part 2 unsuccessful due to runtime issues

--- day16/day16.cpp
+++ day16/day16.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
 #include <unordered_map>
 #include <algorithm>
 #include <chrono>
@@ -9,6 +10,7 @@ using namespace std;
 std::unordered_map<std::string, std::pair<int, std::vector<std::string>>> exploration_space;
 std::vector<std::string> node_names;
 std::vector<std::vector<int>> path_weights;
+std::unordered_map<std::string, int> valve_masks;
 
 std::vector<std::string> extract_exploration(std::string line, std::string &name, int &relief) {
     std::vector<std::string> connections(0);
@@ -39,6 +41,13 @@ void load_space(ifstream &input) {
     while (getline(input, line)) {
         connections = extract_exploration(line, name, relief);
         exploration_space[name] = make_pair(relief, connections);
+    }
+
+    int i = 0;
+    for (std::pair<std::string, std::pair<int, std::vector<std::string>>> point : exploration_space) {
+        if (point.second.first == 0)
+            continue;
+        valve_masks[point.first] = 1<<i++;
     }
 }
 
@@ -88,6 +97,41 @@ int recursive_explore(std::string location, int time_remaining, std::vector<stri
     return score;
 }
 
+std::map<int,int> explore(std::string location, int time_remaining, int visited_state, int current_flow, std::map<int,int> &results) {
+    if (results.find(visited_state) == results.end())
+        results[visited_state] = current_flow;
+    else
+        results[visited_state] = std::max(current_flow, results[visited_state]);
+    
+    int next_time, here_index = std::find(node_names.begin(), node_names.end(), location) - node_names.begin(), there_index;
+    for (std::pair<std::string,int> mask : valve_masks) {
+        there_index = std::find(node_names.begin(), node_names.end(), mask.first) - node_names.begin();
+        next_time = time_remaining - path_weights[here_index][there_index] - 1;
+
+        if (next_time <= 0 || (mask.second & visited_state) != 0)
+            continue;
+        
+        explore(mask.first, next_time, visited_state | mask.second, current_flow + next_time * exploration_space[mask.first].first, results);
+    }
+    
+    return results;
+}
+
+int explore_pt2() {
+    std::map<int,int> results;
+    int result = 0;
+    results = explore("AA", 26, 0, 0, results);
+    for (std::map<int,int>::iterator first = results.begin(); first != results.end(); first++) {
+        for (std::map<int,int>::iterator second = results.begin(); second != results.end(); second++) {
+            if ((first->first & second->first) != 0)
+                continue;
+            
+            result = std::max(result, first->second + second->second);
+        }
+    }
+    return result;
+}
+
 int main() {
     ifstream input("input.txt");
     if (!input.is_open()) {
@@ -109,8 +153,11 @@ int main() {
     }
     
     auto t1 = std::chrono::high_resolution_clock::now();
-    std::cout << "Part 1: " << recursive_explore("AA", 30, non_zeroes) << std::endl;
+    //std::cout << "Part 1: " << recursive_explore("AA", 30, non_zeroes) << std::endl;
     auto t2 = std::chrono::high_resolution_clock::now();
     std::cout << "Execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << std::endl;
+    std::cout << "Part 2: " << explore_pt2() << std::endl;
+    auto t3 = std::chrono::high_resolution_clock::now();
+    std::cout << "Execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2).count() << std::endl;
     return 0;
 }
