a55892fab9007b9685b8be967c1fbc88cfa40427
Alviere <againes0501@gmail.com>
2023-07-02 02:58:46 -0500

    Day 18 part 2 successful

--- day18/day18.cpp
+++ day18/day18.cpp
@@ -2,12 +2,13 @@
 #include <fstream>
 #include <vector>
 #include <array>
-#include <algorithm>
 #include <set>
 using namespace std;
 
+//kludge
+const int max_dim = 100;
 
-std::array<int,3> read_coord(std::string line, int &max_dim) {
+std::array<int,3> read_coord(std::string line) {
     std::array<int,3> result;
     int one, two;
     
@@ -21,31 +22,29 @@ std::array<int,3> read_coord(std::string line, int &max_dim) {
     
     one = two + 1;
     result[2] = stoi(line.substr(one));
-    
-    max_dim = std::max(max_dim, *std::max_element(result.begin(), result.end()));
 
     return result;
 }
 
-std::vector<std::array<int,3>> read_input(ifstream &input, int &max_dim) {
+std::vector<std::array<int,3>> read_input(ifstream &input) {
     std::vector<std::array<int,3>> results;
     std::string line;
     
     while (getline(input, line)) {
-        results.push_back(read_coord(line, max_dim));
+        results.push_back(read_coord(line));
     }
     
     return results;
 }
 
-int coord_to_int(std::array<int,3> coord, int max_dim) {
+int coord_to_int(std::array<int,3> coord) {
     int result = coord[0];
     result = result * max_dim + coord[1];
     result = result * max_dim + coord[2];
     return result;
 }
 
-std::array<int,3> int_to_coord(int value, int max_dim) {
+std::array<int,3> int_to_coord(int value) {
     std::array<int,3> result = {0,0,0};
     result[2] = value % max_dim;
     value /= max_dim;
@@ -55,12 +54,12 @@ std::array<int,3> int_to_coord(int value, int max_dim) {
     return result;
 }
 
-int conversion_checker(std::vector<std::array<int,3>> coords, int max_dim) {
+int conversion_checker(std::vector<std::array<int,3>> coords) {
     int holder;
     std::array<int,3> holderray;
     for (std::array<int,3> coord : coords) {
-        holder = coord_to_int(coord, max_dim);
-        holderray = int_to_coord(holder, max_dim);
+        holder = coord_to_int(coord);
+        holderray = int_to_coord(holder);
         for (int foo = 0; foo < 3; foo++) {
             if (coord[foo] == holderray[foo])
                 continue;
@@ -73,19 +72,19 @@ int conversion_checker(std::vector<std::array<int,3>> coords, int max_dim) {
     return 0;
 }
 
-std::set<int> convert_coords(std::vector<std::array<int,3>> coords, int max_dim) {
+std::set<int> convert_coords(std::vector<std::array<int,3>> coords) {
     std::set<int> results;
     for (std::array<int,3> coord : coords) {
-        results.insert(coord_to_int(coord, max_dim));
+        results.insert(coord_to_int(coord));
     }
     return results;
 }
 
-int count_surfaces(int max_dim, std::set<int> condensed_coords) {
+int count_surfaces(std::set<int> condensed_coords) {
     std::vector<std::array<int,3>> offset_coords = { {-1,0,0}, {1,0,0}, {0,-1,0}, {0,1,0}, {0,0,-1}, {0,0,1} };
     std::vector<int> offset_ints;
     for (std::array<int,3> coord : offset_coords) {
-        offset_ints.push_back(coord_to_int(coord, max_dim));
+        offset_ints.push_back(coord_to_int(coord));
     }
 
     int result = 0;
@@ -99,21 +98,70 @@ int count_surfaces(int max_dim, std::set<int> condensed_coords) {
     return result;
 }
 
+int count_outer_surfaces(std::vector<std::array<int,3>> coords, std::set<int> condensed_coords) {
+    int min_wall = -2, max_wall = 21;
+    std::set<int> walls;
+    for (int x = min_wall; x <= max_wall; x++) {
+        for (int y = min_wall; y <= max_wall; y++) {
+            for (int z = min_wall; z <= max_wall; z++) {
+                if ((x==min_wall || x==max_wall) || (y==min_wall || y==max_wall) || (z==min_wall || z==max_wall))
+                    walls.insert(coord_to_int({x,y,z}));
+            }
+        }
+    }
+    
+    std::vector<std::array<int,3>> offset_coords = { {-1,0,0}, {1,0,0}, {0,-1,0}, {0,1,0}, {0,0,-1}, {0,0,1} };
+    std::vector<int> offset_ints;
+    for (std::array<int,3> coord : offset_coords) {
+        offset_ints.push_back(coord_to_int(coord));
+    }
+
+    std::set<int> air;
+    std::set<int> air_checking = { coord_to_int({-1,-1,-1}) };
+    int current_coord;
+    while (air_checking.size() > 0) {
+        current_coord = *air_checking.begin();
+        air_checking.erase(current_coord);
+        air.insert(current_coord);
+
+        for (int offset : offset_ints) {
+            if (walls.count(current_coord + offset) != 0 || condensed_coords.count(current_coord + offset) != 0)
+                continue;
+            
+            if (air.count(current_coord + offset) != 0)
+                continue;
+            
+            if (air_checking.count(current_coord + offset) != 0)
+                continue;
+
+            air_checking.insert(current_coord + offset);
+        }
+    }
+
+    int result = 0;
+    for (int current_coord : condensed_coords) {
+        for (int offset : offset_ints) {
+            if (air.count(current_coord + offset) > 0)
+                result++;
+        }
+    }
+    return result;
+}
+
 int main() {
     ifstream input("input.txt");
     if (!input.is_open()) {
         std::cout << "Unable to open file" << std::endl;
         return -1;
     }
-    int max_dim = 0;
-    std::vector<std::array<int,3>> coords = read_input(input, max_dim);
+    std::vector<std::array<int,3>> coords = read_input(input);
     input.close();
-    max_dim++;
-    if (conversion_checker(coords, max_dim) != 0)
+    if (conversion_checker(coords) != 0)
         return -1;
     
-    std::set<int> condensed_coords = convert_coords(coords, max_dim);
+    std::set<int> condensed_coords = convert_coords(coords);
     
-    std::cout << "Part 1: " << count_surfaces(max_dim, condensed_coords) << std::endl;
+    std::cout << "Part 1: " << count_surfaces(condensed_coords) << std::endl;
+    std::cout << "Part 2: " << count_outer_surfaces(coords, condensed_coords) << std::endl;
     return 0;
 }
