20eb6c20250b0a0f03970efe6f71d7fa325de753
Alviere <againes0501@gmail.com>
2023-07-09 13:02:29 -0500

    Day 20 part 2 successful

--- day20/day20.cpp
+++ day20/day20.cpp
@@ -12,33 +12,39 @@ std::vector<int> read_input(ifstream &input) {
     return results;
 }
 
-std::vector<int> mix(std::vector<int> input) {
-    std::vector<std::pair<int,int>> mixed;
+std::vector<long long> mix(std::vector<int> input, int rounds, long long key) {
+    std::vector<std::pair<int, int>> mixed;
     for (int i = 0; i < input.size(); i++)
         mixed.push_back(make_pair(i, input[i]));
     
     int old_index, new_index, modul = input.size() - 1;
     std::pair<int,int> value;
-    for (int i = 0; i < input.size(); i++) {
-        value = make_pair(i, input[i]);
-        old_index = std::find(mixed.begin(), mixed.end(), value) - mixed.begin();
-        mixed.erase(mixed.begin() + old_index);
-        new_index = ((old_index + value.second) % modul + modul) % modul;
-        mixed.insert(mixed.begin() + new_index, value);
+    for (int round = 0; round < rounds; round++) {
+        for (int i = 0; i < input.size(); i++) {
+            value = make_pair(i, input[i]);
+            old_index = std::find(mixed.begin(), mixed.end(), value) - mixed.begin();
+            mixed.erase(mixed.begin() + old_index);
+            new_index = ((key * value.second + old_index) % modul + modul) % modul;
+            mixed.insert(mixed.begin() + new_index, value);
+        }
     }
+    
+    std::vector<long long> output;
+    for (int i = 0; i < mixed.size(); i++)
+        output.push_back(key * mixed[i].second);
 
-    for (int i = 0; i < input.size(); i++) {
-        input[i] = mixed[i].second;
-    }
-
-    return input;
+    return output;
 }
 
-int part1_calculate(std::vector<int> mixed_input) {
-    int results = 0, index, modul = mixed_input.size();
-    index = std::find(mixed_input.begin(), mixed_input.end(), 0) - mixed_input.begin();
+long long extract_coords(std::vector<int> input_list, int rounds, long long key) {
+    std::vector<long long> mixed = mix(input_list, rounds, key);
+    
+    int index, modul = input_list.size();
+    long long results = 0;
+    
+    index = std::find(mixed.begin(), mixed.end(), 0) - mixed.begin();
     for (int value : {1000,2000,3000}) {
-        results += mixed_input[(index + value) % modul];
+        results += mixed[(index + value) % modul];
     }
     return results;
 }
@@ -52,8 +58,8 @@ int main() {
     std::vector<int> input_list = read_input(input);
     input.close();
 
-    std::vector<int> one_mix = mix(input_list);
-    std::cout << "Part 1: " << part1_calculate(one_mix) << std::endl;
+    std::cout << "Part 1: " << extract_coords(input_list, 1, 1) << std::endl;
+    std::cout << "Part 2: " << extract_coords(input_list, 10, 811589153) << std::endl;
     
     return 0;
 }
