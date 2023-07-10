184f7987654492ae777288430aacd65019a79ef8
Alviere <againes0501@gmail.com>
2023-07-09 21:36:04 -0500

    Day 21 both parts successful, optimization (2.166 speedup)

--- day21/day21.cpp
+++ day21/day21.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <map>
 #include <tuple>
+#include <chrono>
 using namespace std;
 
 typedef std::tuple<std::string, std::string, char> func_monkey_data;
@@ -90,6 +91,21 @@ long long part2_find() {
     return int_monkeys["humn"];
 }
 
+bool collapse_except(std::string current, std::string ignore) {
+    if (current.compare(ignore) == 0)
+        return false; //if current is the ignored one, say it couldn't collapse
+    
+    if (func_monkeys.count(current) == 0)
+        return true; //if current isn't a function monkey, say it's collapsed
+    
+    if (!collapse_except(std::get<0>(func_monkeys[current]), ignore) || !collapse_except(std::get<1>(func_monkeys[current]), ignore))
+        return false; //if a child couldn't collapse, say this one can't collapse
+    
+    int_monkeys[current] = read_monkey(current);
+    func_monkeys.erase(current);
+    return true;
+}
+
 int main() {
     ifstream input("input.txt");
     if (!input.is_open()) {
@@ -99,9 +115,24 @@ int main() {
     load_monkeys(input);
     input.close();
     
-    long long part1 = read_monkey("root");
-    std::cout << "Part 1: " << part1 << std::endl;
+    auto t1 = std::chrono::high_resolution_clock::now();    
+    collapse_except("root", "humn");
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> exec_double = t2 - t1;
+    std::cout << "Collapse execution time: " << exec_double.count() << "ms" << std::endl;
+    
+    t1 = std::chrono::high_resolution_clock::now();
+    std::cout << "Part 1: " << read_monkey("root") << std::endl;
+    t2 = std::chrono::high_resolution_clock::now();
+    exec_double = t2 - t1;
+    std::cout << "Part 1 execution time: " << exec_double.count() << "ms" << std::endl;
+    
+    
+    t1 = std::chrono::high_resolution_clock::now();
     std::cout << "Part 2: " << part2_find() << std::endl;
+    t2 = std::chrono::high_resolution_clock::now();
+    exec_double = t2 - t1;
+    std::cout << "Part 2 execution time: " << exec_double.count() << "ms" << std::endl;
     
     return 0;
 }
