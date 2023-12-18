6eed4add2878b9cfa7613dc9b559435a1640d18f
Alviere <againes0501@gmail.com>
2023-12-18 15:29:37 -0800

    Day 07 pt2 successful

--- day07/day07.py
+++ day07/day07.py
@@ -2,12 +2,11 @@
 from functools import cmp_to_key, reduce
 import operator
 
-def classify(card):
+def classify_pt1(card):
     counts = dict()
     for char in card:
         counts[char] = 1 + counts.get(char, 0)
-    counts = list(counts.values())
-    counts.sort()
+    counts = sorted(counts[char] for char in counts)
     match counts:
         case [5]:
             return 6
@@ -23,26 +22,52 @@ def classify(card):
             return 1
         case [1,1,1,1,1]:
             return 0
-        
-    return -1
-input = {card:{"bid":bid, "class":classify(card)} for card,bid in (item.split() for item in open("day07/day07.txt"))}
-#print(input)
+        case _:
+            print("UH OH")
 
-char_ranks = ['A','K','Q','J','T','9','8','7','6','5','4','3','2']
-def compare(card1, card2):
-    relative = input[card1]["class"] - input[card2]["class"]
-    if relative != 0:
-        return relative
-    #first nonmatching character -> grade
-    for char1,char2 in zip(card1, card2):
-        if char1 != char2:
-            break
+def classify_pt2(card):
+    counts = dict()
+    for char in card:
+        counts[char] = 1 + counts.get(char, 0)
+    counts_sorted = sorted(counts[char] for char in counts if char != 'J')
+    if len(counts_sorted) > 0:
+        counts_sorted[-1] += counts.get('J',0)
     else:
-        return 0
-    
-    return char_ranks.index(char2) - char_ranks.index(char1)
-    
-    
-card_list = list(input.keys())
-card_list.sort(key=cmp_to_key(compare))
-print(reduce(operator.add, (index*int(input[card]["bid"]) for index,card in enumerate(card_list, start=1))))
+        counts_sorted.append(counts.get('J',0))
+    match counts_sorted:
+        case [5]:
+            return 6
+        case [1,4]:
+            return 5
+        case [2,3]:
+            return 4
+        case [1,1,3]:
+            return 3
+        case [1,2,2]:
+            return 2
+        case [1,1,1,2]:
+            return 1
+        case [1,1,1,1,1]:
+            return 0
+        case _:
+            print("UH OH")
+
+def process(hand_classifier, card_rankings):
+    def compare(card1, card2):
+        relative = hand_classifier(card1) - hand_classifier(card2)
+        if relative != 0:
+            return relative
+        #first nonmatching character -> grade
+        for char1,char2 in zip(card1, card2):
+            if char1 != char2:
+                break
+        else:
+            return 0
+        
+        return card_rankings.index(char2) - card_rankings.index(char1)
+    comparator = cmp_to_key(compare)
+    print(reduce(operator.add, (index*int(bids[card]) for index,card in enumerate(sorted(bids.keys(), key=comparator), start=1))))
+
+bids = {card:bid for card,bid in (item.split() for item in open("day07/day07.txt"))}
+process(classify_pt1, ['A','K','Q','J','T','9','8','7','6','5','4','3','2'])
+process(classify_pt2, ['A','K','Q','T','9','8','7','6','5','4','3','2','J'])
