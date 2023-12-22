04754bdcc7b44d7a9291dee63c1597793bf6473c
Alviere <againes0501@gmail.com>
2023-12-22 13:13:48 -0800

    Added iterative non-memoized form, 30% speedup of pt1, 80% speedup of pt2

--- day12/day12.py
+++ day12/day12.py
@@ -29,6 +29,21 @@ def recursive_nonagram_row(record, notes, memo=dict()):
     memo[record,notes] = results
     return results
 
+def iterative_nonagram_row(record, notes):
+    blocks_and_paths = {0:1}
+    for note_index, note_value in enumerate(notes):
+        new_blocks_and_paths = dict()
+        for block_start, paths_count in blocks_and_paths.items():
+            for index in range(block_start, len(record) - sum(notes[note_index + 1:]) - len(notes[note_index + 1:])):
+                if ((index+note_value <= len(record) and '.' not in record[index:index+note_value]) 
+                    and (index+note_value == len(record) or record[index+note_value] != '#') 
+                    and (note_index < len(notes) - 1 or '#' not in record[index + note_value + 1:])):
+                    new_blocks_and_paths[index+note_value+1] = paths_count + new_blocks_and_paths.get(index+note_value+1,0)
+                if record[index] == '#':
+                    break
+        blocks_and_paths = new_blocks_and_paths
+    return sum(blocks_and_paths.values())
+
 result = 0
 start_time = time.time()
 for record, notes in records:
@@ -37,10 +52,26 @@ end_time = time.time()
 print(end_time-start_time)
 print(result)
 
+result_2 = 0
+start_time = time.time()
+for record, notes in records:
+    result_2 += iterative_nonagram_row(compress(record), notes)
+end_time = time.time()
+print(end_time-start_time)
+print(result_2)
+
 second_result = 0
 start_time = time.time()
 for record,notes in tqdm(records):
-    second_result += recursive_nonagram_row("?".join([record]*5), tuple(notes*5))
+    second_result += recursive_nonagram_row(compress("?".join([record]*5)), tuple(notes*5))
 end_time = time.time()
 print(end_time-start_time)
 print(second_result)
+
+second_result_2 = 0
+start_time = time.time()
+for record, notes in records:
+    second_result_2 += iterative_nonagram_row(compress("?".join([record]*5)), notes*5)
+end_time = time.time()
+print(end_time-start_time)
+print(second_result_2)
