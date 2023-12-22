9858082016520230a9d892bcb1ed64183e8b906c
Alviere <againes0501@gmail.com>
2023-12-22 12:08:51 -0800

    Day 12 both parts, added memoization, pt1 0.03155s->0.02746s, pt2 1.44s

--- day12/day12.py
+++ day12/day12.py
@@ -1,4 +1,7 @@
 
+from tqdm import tqdm
+import time
+
 def compress(record_string: str):
     result_list = []
     for char in record_string.strip('.'):
@@ -9,7 +12,9 @@ def compress(record_string: str):
 
 records = [(row[0], [int(item) for item in row[1].split(',')]) for row in (line.split() for line in open("day12/day12.txt"))]
 
-def recursive_nonagram_row(record, notes):
+def recursive_nonagram_row(record, notes, memo=dict()):
+    if (record,notes) in memo:
+        return memo[record,notes]
     if ((len(notes) == 0 and (len(record) == 0 or '#' not in record))
         or (len(notes) == 1 and len(record) == notes[0] and '.' not in record)):
             return 1
@@ -18,12 +23,24 @@ def recursive_nonagram_row(record, notes):
     results = 0
     for index in range(len(record) - sum(notes[1:]) - len(notes[1:])):
         if (index+notes[0] <= len(record) and '.' not in record[index:index+notes[0]]) and (index+notes[0] == len(record) or record[index+notes[0]] != '#'):
-            results += recursive_nonagram_row(compress(record[index+notes[0]+1:]), notes[1:])
+            results += recursive_nonagram_row(compress(record[index+notes[0]+1:]), notes[1:], memo)
         if record[index] == '#':
             break
+    memo[record,notes] = results
     return results
 
 result = 0
+start_time = time.time()
 for record, notes in records:
-    result += recursive_nonagram_row(compress(record), notes)
+    result += recursive_nonagram_row(compress(record), tuple(notes))
+end_time = time.time()
+print(end_time-start_time)
 print(result)
+
+second_result = 0
+start_time = time.time()
+for record,notes in tqdm(records):
+    second_result += recursive_nonagram_row("?".join([record]*5), tuple(notes*5))
+end_time = time.time()
+print(end_time-start_time)
+print(second_result)
