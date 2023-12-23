948e32e1a6e865600fb01cc3863a6de75d0772e9
Alviere <againes0501@gmail.com>
2023-12-23 10:17:36 -0800

    Refactoring, day 14 pt1 successful

--- day14/day14.py
+++ day14/day14.py
@@ -1,32 +1,29 @@
 
 stationary_rocks = set()
-rolling_rocks = dict()
+rolling_rocks = set()
 total_row = 0
 width = 0
 for line in open("day14/day14.txt"):
     width = max(width, len(line.strip()))
-    rolling_rocks[total_row] = set()
     for x,char in enumerate(line.strip()):
         match char:
             case '#':
-                stationary_rocks.add((x,total_row))
+                stationary_rocks.add(x + total_row*1j)
             case 'O':
-                rolling_rocks[total_row].add(x)
+                rolling_rocks.add(x + total_row*1j)
     total_row += 1
 
-for row in rolling_rocks:
-    for rock in list(rolling_rocks[row]):
-        rolling_rocks[row].remove(rock)
-        new_row = row - 1
-        while new_row >= 0:
-            if (rock,new_row) in stationary_rocks:
-                break
-            if rock in rolling_rocks[new_row]:
-                break
-            new_row -= 1
-        rolling_rocks[new_row+1].add(rock)
+def shift(direction_offset):
+    for rock in list(rolling_rocks):
+        rolling_rocks.remove(rock)
+        while rock.real >= 0 and rock.real < total_row and rock.imag >= 0 and rock.imag < width and rock not in stationary_rocks:
+            if rock not in rolling_rocks:
+                room_to_go = rock
+            rock += direction_offset
+        rolling_rocks.add(room_to_go)
 
 result = 0
-for row in rolling_rocks:
-    result += (total_row - row) * len(rolling_rocks[row])
+shift(-1j)
+for rock in rolling_rocks:
+    result += total_row - int(rock.imag)
 print(result)
