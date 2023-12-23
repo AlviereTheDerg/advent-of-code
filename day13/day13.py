c16a82376f450bd6eb16f79347fb61426753611d
Alviere <againes0501@gmail.com>
2023-12-22 22:38:36 -0800

    Day 13 pt2 successful, pt1 nixed

--- day13/day13.py
+++ day13/day13.py
@@ -6,7 +6,7 @@ for line in open("day13/day13.txt"):
     else:
         rock_locations[-1].append(line.strip())
 
-result = 0
+rock_maps = []
 for block in rock_locations:
     #prepare special notations
     coords = set()
@@ -19,38 +19,42 @@ for block in rock_locations:
             coords.add((x,y))
             rows[y].add(x)
             cols[x].add(y)
-    #print("coords", coords)
-    #print("rows", rows)
-    #print("cols", cols)
+    rock_maps.append((rows, cols))
 
+result = 0
+for rows, cols in rock_maps:
     rows_above = -1
-    row_mirror_possibles = [(index,index+1) for index in rows if index+1 in rows and rows[index] == rows[index+1]]
-    for possibility in row_mirror_possibles:
-        offset = 1
+    for possibility in ((index,index+1) for index in rows if index+1 in rows):
+        smudges = 1
+        offset = 0
         while possibility[0] - offset in rows and possibility[1] + offset in rows:
             blocked_ups = rows[possibility[0] - offset]
             blocked_downs = rows[possibility[1] + offset]
-            if blocked_ups != blocked_downs:
+            smudges -= len(blocked_ups ^ blocked_downs)
+            if smudges < 0:
                 break
             offset += 1
         else:
-            rows_above = possibility[0]
-            break
+            if smudges == 0:
+                rows_above = possibility[0]
+                break
     #print(rows_above)
 
     cols_left = -1
-    col_mirror_possibles = [(index,index+1) for index in cols if index+1 in cols and cols[index] == cols[index+1]]
-    for possibility in col_mirror_possibles:
-        offset = 1
+    for possibility in ((index,index+1) for index in cols if index+1 in cols):
+        smudges = 1
+        offset = 0
         while possibility[0] - offset in cols and possibility[1] + offset in cols:
             blocked_lefts = cols[possibility[0] - offset]
             blocked_rights = cols[possibility[1] + offset]
-            if blocked_lefts != blocked_rights:
+            smudges -= len(blocked_lefts ^ blocked_rights)
+            if smudges < 0:
                 break
             offset += 1
         else:
-            cols_left = possibility[0]
-            break
+            if smudges == 0:
+                cols_left = possibility[0]
+                break
     #print(cols_left)
     #print(row_mirror_possibles)
     #print(col_mirror_possibles)
