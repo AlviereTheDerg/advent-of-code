dabf1da2d73f664d8677dc84809513384a97aa16
Alviere <againes0501@gmail.com>
2023-12-23 23:24:59 -0800

    Day 18 both parts successful

--- day18/day18.py
+++ day18/day18.py
@@ -1,16 +1,20 @@
 
-direction_translate = {'R':1, 'D':1j, 'L':-1, 'U':-1j}
-input_commands = [(direction_translate[direction],int(distance),colour[1:-1]) for direction,distance,colour in 
+input_commands = [(direction,distance,colour[1:-1]) for direction,distance,colour in 
                   (line.strip().split(' ') for line in open("day18/day18.txt"))]
-#print(input_commands)
 
-node_circuit = [0]
-for offset,distance,_ in input_commands:
-    for _ in range(distance):
-        node_circuit.append(node_circuit[-1] + offset)
+def get_lagoon_size(dig_instructions):
+    node_count = 1
+    area = 0
+    current_node = 0
+    for offset,distance in dig_instructions:
+        node_count += distance
+        next_node = current_node + offset * distance
+        area += current_node.real * next_node.imag - current_node.imag * next_node.real
+        current_node = next_node
+    
+    area = 0.5 * int(area)
+    internals = int(area) + 1 - node_count // 2
+    print(node_count-1 + internals)
 
-area = 0.5 * abs(sum((first.real * second.imag - first.imag * second.real for first,second in 
-                      ((node_circuit[x], node_circuit[(x+1)%len(node_circuit)]) for x in range(len(node_circuit))))))
-#print(area)
-internals = int(area) + 1 - len(node_circuit) // 2
-print(len(node_circuit)-1 + internals)
+get_lagoon_size(({'R':1, 'D':1j, 'L':-1, 'U':-1j}[offset],int(distance)) for offset,distance,_ in input_commands)
+get_lagoon_size(({'0':1, '1':1j, '2':-1, '3':-1j}[colour[-1]],int(colour[1:-1],16)) for _,_,colour in input_commands)
