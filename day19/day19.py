0d8e7b45f2c06579b2f9190bd49434e1e58b57cb
Alviere <againes0501@gmail.com>
2023-12-24 21:54:42 -0800

    Day 19 refactoring, pt1 still works

--- day19/day19.py
+++ day19/day19.py
@@ -1,36 +1,45 @@
 
-x,m,a,s = "xmas"
 file = open("day19/day19.txt")
+def process(workflow, item):
+    for destination, determinator in workflow:
+        if determinator == True:
+            return destination
+        category, comparator, bound = determinator
+        if comparator == '<':
+            if item[category] < bound:
+                return destination
+        else:
+            if item[category] > bound:
+                return destination
+
 workflows = dict()
 while (line := file.readline()) != '\n':
     label = line[:line.find('{')]
-    def generate_rules(line):
-        rules = []
-        for rule in line[line.find('{')+1:-2].split(','):
-            if ':' not in rule:
-                rules.append((lambda item: True, rule))
-            else:
-                rules.append((lambda item, rule=rule: 
-                                item[rule[0]] < int(rule[2:rule.find(':')]) 
-                                if rule[1]=='<' else 
-                                item[rule[0]] > int(rule[2:rule.find(':')]), 
-                              rule[rule.find(':')+1:]))
-        def process(item):
-            for rule,destination in rules:
-                if rule(item):
-                    return destination
-        return process
-    workflows[label] = generate_rules(line)
+    rules = []
+    for rule in line[line.find('{')+1:-2].split(','):
+        if ':' not in rule:
+            rules.append((rule, True))
+        else:
+            destination = rule[rule.find(':')+1:]
+            category = rule[0]
+            comparator = rule[1]
+            bound = int(rule[2:rule.find(':')])
+            rules.append((destination, (category, comparator, bound)))
+    workflows[label] = rules
 
 item_list = []
 while (line := file.readline()):
-    item_list.append(eval(line.strip().replace('=', ':')))
+    item = dict()
+    
+    for chunk in line[1:-2].split(','):
+        item[chunk[0]] = int(chunk[2:])
+    item_list.append(item)
 
 item_map = {"in":item_list}
 while len(item_map.keys() - {'A','R'}) > 0:
     this_label = next(iter(item_map.keys() - {'A','R'}))
     for item in item_map.pop(this_label):
-        that_label = workflows[this_label](item)
+        that_label = process(workflows[this_label], item)
         if this_label == that_label:
             print("OH GOD OH FUCK")
             exit()
@@ -40,4 +49,4 @@ while len(item_map.keys() - {'A','R'}) > 0:
 
 #print("Accepted:", item_map.get('A', None))
 #print("Rejected:", item_map.get('R', None))
-print(sum(item[x]+item[m]+item[a]+item[s] for item in item_map.get('A',[])))
+print(sum(item['x']+item['m']+item['a']+item['s'] for item in item_map.get('A',[])))
