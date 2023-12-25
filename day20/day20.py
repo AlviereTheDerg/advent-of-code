70034136db4be9f2be0f12efd254add6d4baa5d9
Alviere <againes0501@gmail.com>
2023-12-25 12:06:40 -0800

    Day 20 both parts successful

--- day20/day20.py
+++ day20/day20.py
@@ -2,6 +2,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 from collections import deque
+from copy import deepcopy
 
 connections = nx.DiGraph()
 for line in open("day20/day20.txt"):
@@ -16,28 +17,29 @@ for line in open("day20/day20.txt"):
     for neighbour in line[1].split(', '):
         connections.add_edge(this_node, neighbour)
 
-# setup
-status = dict()
-for node in connections.nodes():
-    match connections.nodes[node].get('behavior'):
-        case '%':
-            status[node] = 0
-        case '&':
-            this_node_status = dict()
-            for neighbour in connections.predecessors(node):
-                this_node_status[neighbour] = 0
-            status[node] = this_node_status
-        case _:
-            pass
+def initialize(graph: nx.DiGraph):
+    status = dict()
+    for node in graph.nodes():
+        match graph.nodes[node].get('behavior'):
+            case '%':
+                status[node] = 0
+            case '&':
+                this_node_status = dict()
+                for neighbour in graph.predecessors(node):
+                    this_node_status[neighbour] = 0
+                status[node] = this_node_status
+            case _:
+                pass
+    return status
 
-def button_press():
+def button_press(status, graph):
     sent = {0:1, 1:0}
     propagation_queue = deque()
     propagation_queue.append(('broadcaster', 0, 'button'))
     while propagation_queue:
         reciever,signal,sender = propagation_queue.popleft()
         #print(sender,signal,reciever)
-        match connections.nodes[reciever].get('behavior'):
+        match graph.nodes[reciever].get('behavior'):
             case '%':
                 if signal == 1:
                     continue
@@ -52,21 +54,46 @@ def button_press():
             case 'broadcaster':
                 sending = signal
             case _:
-                continue
-        for downstream in connections[reciever]:
+                graph.add_node(reciever, recieved=graph.nodes[reciever].get('recieved',set())|{signal})
+        for downstream in graph[reciever]:
             sent[sending] += 1
             propagation_queue.append((downstream, sending, reciever))
     return sent
 
-#pos = nx.spring_layout(connections)
-#nx.draw_networkx_nodes(connections, pos)
-#nx.draw_networkx_edges(connections, pos)
-#nx.draw_networkx_labels(connections, pos)
-#plt.show()
-
 total_presses = {0:0,1:0}
+status = initialize(connections)
 for _ in range(1000):
-    for signal,count in button_press().items():
+    for signal,count in button_press(status, connections).items():
         total_presses[signal] += count
-print(total_presses)
+#print(total_presses)
 print(total_presses[0] * total_presses[1])
+
+con_parent = next(connections.predecessors("rx"))
+predecessor_graphs = dict()
+predecessors = set(connections.predecessors(con_parent))
+total_presses = 1
+for predecessor in predecessors:
+    predecessor_graph = deepcopy(connections)
+    purge_list = deque(predecessors - {predecessor})
+
+    while purge_list:
+        next_purge = purge_list.popleft()
+        if next_purge not in predecessor_graph:
+            continue
+        for parent in predecessor_graph.predecessors(next_purge):
+            if parent == 'broadcaster':
+                continue
+            purge_list.append(parent)
+        predecessor_graph.remove_node(next_purge)
+    parent = next(predecessor_graph.predecessors(predecessor))
+    predecessor_graph.add_edge(parent, "rx")
+    predecessor_graph.remove_nodes_from((con_parent, predecessor))
+
+    status = initialize(predecessor_graph)
+    presses = 0
+    while 0 not in predecessor_graph.nodes["rx"].get('recieved'):
+        presses += 1
+        button_press(status, predecessor_graph)
+    #print(presses)
+    total_presses *= presses
+print(total_presses)
