586fa4fdeab0131448178fe4443b954430e24cb8
Alviere <againes0501@gmail.com>
2024-05-22 21:34:36 -0700

    Day 3 part 2 successful

--- 2021/day03/day03.java
+++ 2021/day03/day03.java
@@ -2,10 +2,23 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class day03 {
+    public static int extract_life_support(List<String> data, Predicate<Integer> filter) {
+        for (AtomicInteger index = new AtomicInteger(); index.get() < data.get(0).length(); index.incrementAndGet()) {
+            char match = filter.test(data.stream().mapToInt((s) -> s.charAt(index.get()) == '1' ? 1 : -1).sum()) ? '1' : '0';
+            data = data.stream().filter((s) -> s.charAt(index.get()) == match).toList();
+            if (data.size() == 1) {
+                return Integer.parseInt(data.get(0), 2);
+            }
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
         try {
             // input file -> list of Integer
@@ -23,6 +36,8 @@ public class day03 {
             int gamma    = Integer.parseInt(commons.stream().map((x) -> x ? "1" : "0").collect(Collectors.joining()), 2);
             int epsilon  = Integer.parseInt(commons.stream().map((x) -> x ? "0" : "1").collect(Collectors.joining()), 2);
             System.out.println(gamma * epsilon);
+
+            System.out.println(extract_life_support(data, x -> x >= 0) * extract_life_support(data, x -> x < 0));
 
         } catch (Exception e) {
             System.out.println(e.toString());
