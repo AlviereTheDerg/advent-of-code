3e76c088b26720eb1c4c98d611bc6a0a2b760c6e
Alviere <againes0501@gmail.com>
2024-06-02 06:32:32 -0700

    Day 6 part 2 successful

--- 2021/day06/day06.java
+++ 2021/day06/day06.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class day06 {
+
     public static void main(String[] args) {
         try {
             File input_file = new File("2021/day06/day06.txt");
@@ -14,22 +15,30 @@ public class day06 {
             List<Integer> fish_input = Arrays.stream(input_scanner.next().split(",")).map(Integer::parseInt).toList();
             input_scanner.close();
 
-            Map<Integer, Integer> fish_ages = new HashMap<>();
+            Map<Integer, Long> fish_ages = new HashMap<>();
             for (int fish : fish_input) {
                 if (!fish_ages.containsKey(fish))
-                    fish_ages.put(fish, 0);
+                    fish_ages.put(fish, 0l);
                 fish_ages.put(fish, fish_ages.get(fish) + 1);
             }
 
-            int max_cycles = 80, fish_spawn_cycle = 7, fish_new_buffer = 2;
-            for (int i = 0; i < max_cycles; i++) {
+            int pt1_cycles = 80, fish_spawn_cycle = 7, fish_new_buffer = 2;
+            for (int i = 0; i < pt1_cycles; i++) {
                 int next_fish_cycle = i + fish_spawn_cycle, new_fish_cycle = i + fish_spawn_cycle + fish_new_buffer;
-                int this_fish_count = fish_ages.getOrDefault(i, 0);
-                fish_ages.put(next_fish_cycle, this_fish_count + fish_ages.getOrDefault(next_fish_cycle, 0));
-                fish_ages.put(new_fish_cycle, this_fish_count + fish_ages.getOrDefault(new_fish_cycle, 0));
+                long this_fish_count = fish_ages.getOrDefault(i, 0l);
+                fish_ages.put(next_fish_cycle, this_fish_count + fish_ages.getOrDefault(next_fish_cycle, 0l));
+                fish_ages.put(new_fish_cycle, this_fish_count + fish_ages.getOrDefault(new_fish_cycle, 0l));
             }
-            int fish_count = fish_ages.entrySet().stream().mapToInt(x -> x.getKey() >= max_cycles ? x.getValue() : 0).sum();
-            System.out.println(fish_count);
+            System.out.println(fish_ages.entrySet().stream().mapToLong(x -> x.getKey() >= pt1_cycles ? x.getValue() : 0).sum());
+
+            int pt2_cycles = 256;
+            for (int i = pt1_cycles; i < pt2_cycles; i++) {
+                int next_fish_cycle = i + fish_spawn_cycle, new_fish_cycle = i + fish_spawn_cycle + fish_new_buffer;
+                long this_fish_count = fish_ages.getOrDefault(i, 0l);
+                fish_ages.put(next_fish_cycle, this_fish_count + fish_ages.getOrDefault(next_fish_cycle, 0l));
+                fish_ages.put(new_fish_cycle, this_fish_count + fish_ages.getOrDefault(new_fish_cycle, 0l));
+            }
+            System.out.println(fish_ages.entrySet().stream().mapToLong(x -> x.getKey() >= pt2_cycles ? x.getValue() : 0).sum());
         } catch (Exception e) {
             System.out.println(e.toString());
         }
