42efb60525b050893148765518db7a107135c6b1
Alviere <againes0501@gmail.com>
2024-08-05 04:12:12 -0700

    Day 24 part 1 successful

---
+++ 2021/day24/day24.java
@@ -0,0 +1,99 @@
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class day24 {
+    private static class MONADStage {
+        int A, B, C;
+
+        public MONADStage(List<String> raw_input) {
+            // only 3 values differ between each 'stage' of the process
+            // simplifies down to:
+            /*
+             * w = input
+             * if (w == z%26 + B)
+             *   z /= A
+             * else
+             *   z /= A
+             *   z *= 26
+             *   z += w + C
+             */
+            A = Integer.parseInt(raw_input.get(4).substring(6));
+            B = Integer.parseInt(raw_input.get(5).substring(6));
+            C = Integer.parseInt(raw_input.get(15).substring(6));
+        }
+
+        public int process(int z, int w) {
+            int x = (w == (z % 26) + B) ? 0 : 1;
+            z /= A;
+            z *= (25 * x) + 1;
+            z += (w + C) * x;
+            return z;
+        }
+    }
+
+    public static long part_1(List<MONADStage> stages) {
+        // z is a stack
+        // if A==1 OR w check fails: push w+C to stack
+        // otherwise pop
+        // half of MONADStages have A==1, thus w check MUST PASS WHEN ABLE TO
+        // w check: head of stack == w-B
+        // goal: z is empty
+
+        // identify pairs of 'stage number where value pushed' vs 'stage number where value popped'
+        HashMap<Integer, Integer> pairings = new HashMap<>();
+        LinkedList<Integer> identification_stack = new LinkedList<>();
+        for (int i = 0; i < stages.size(); i++) {
+            if (stages.get(i).A == 1) {
+                identification_stack.push(i);
+            } else {
+                pairings.put(identification_stack.pop(), i);
+            }
+        }
+
+        // construct max model number from the pairs and their offsets
+        int[] max_model = new int[stages.size()];
+        for (Map.Entry<Integer,Integer> entry : pairings.entrySet()) {
+            int difference = stages.get(entry.getKey()).C + stages.get(entry.getValue()).B;
+            if (difference > 0) {
+                max_model[entry.getKey()] = 9 - difference;
+                max_model[entry.getValue()] = 9;
+            } else {
+                max_model[entry.getKey()] = 9;
+                max_model[entry.getValue()] = 9 + difference;
+            }
+        }
+        long z = 0;
+        for (int i = 0; i < stages.size(); i++)
+            z = 10*z + max_model[i];
+        return z;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Scanner input_scanner = new Scanner(new File("2021/day24/day24.txt"));
+            List<String> input_buffer = new ArrayList<>();
+            List<MONADStage> stages = new ArrayList<>();
+            while (input_scanner.hasNext()) {
+                String buffer = input_scanner.nextLine();
+                if (input_buffer.size() > 0 && buffer.equals("inp w")) {
+                    stages.add(new MONADStage(input_buffer));
+                    input_buffer.clear();
+                }
+                input_buffer.add(buffer);
+            }
+            stages.add(new MONADStage(input_buffer));
+            input_scanner.close();
+
+            System.out.println(part_1(stages));
+
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+}
