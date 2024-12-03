e1162fb3ffc5b8d47f33a26863d33c22ff109aa8
Alviere <againes0501@gmail.com>
2024-12-02 18:13:30 -0800

    Day 1 part 1 successful

--- 2024/src/day01.rs
+++ 2024/src/day01.rs
@@ -1,5 +1,18 @@
 
+fn part1(input: &str) {
+    let mut list1 = Vec::new();
+    let mut list2 = Vec::new();
+    for (index, entry) in input.split_whitespace().enumerate() {
+        if index % 2 == 0 {&mut list1} else {&mut list2}
+            .push(entry.parse::<isize>().unwrap());
+    }
+
+    list1.sort(); list2.sort();
+    let result: isize = list1.iter().zip(list2.iter()).map(|(a,b)| (a-b).abs()).sum();
+    println!("{result}");
+}
 
 pub fn main() {
-    println!("{}", crate::grab_input("placeholder"));
+    let input = crate::grab_input("day01");
+    part1(&input);
 }
