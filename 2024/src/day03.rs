2ad1222b2c21fa811ff464fd50f70681b1e4ac0c
Alviere <againes0501@gmail.com>
2024-12-02 21:18:00 -0800

    Day 3 part 2 successful

--- 2024/src/day03.rs
+++ 2024/src/day03.rs
@@ -13,7 +13,33 @@ fn part1(input: &str) {
     println!("{result}");
 }
 
+fn part2(input: &str) {
+    let capturer = Regex::new(r#"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)"#).expect("Failure to construct regex");
+    
+    let mut result = 0;
+    let mut flag = true;
+    for capture in capturer.captures_iter(input) {
+        match capture.get(0) {
+            None => {},
+            Some(stuff) => {
+                match stuff.as_str() {
+                    "do()" => flag = true,
+                    "don't()" => flag = false,
+                    _ => {
+                        if flag {
+                            result += capture.get(1).unwrap().as_str().parse::<isize>().unwrap() * capture.get(2).unwrap().as_str().parse::<isize>().unwrap()
+                        }
+                    },
+                }
+            }
+        }
+    }
+
+    println!("{result}");
+}
+
 pub fn main() {
     let input = crate::grab_input("day03");
     part1(&input);
+    part2(&input);
 }
