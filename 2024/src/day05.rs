7dca54fef00e6729b809195fa1d8c042f320b0bc
Alviere <againes0501@gmail.com>
2024-12-04 21:51:53 -0800

    Day 5 cleanup

--- 2024/src/day05.rs
+++ 2024/src/day05.rs
@@ -1,16 +1,15 @@
 
 use std::collections::HashSet;
 
-fn order(order_rules: &Vec<Vec<isize>>, print: &Vec<isize>) -> bool {
+fn order(order_rules: &Vec<(isize, isize)>, print: &Vec<isize>) -> bool {
     let mut pre_printed: HashSet<&isize> = HashSet::new();
     for page in print.iter() {
         pre_printed.insert(page);
 
-        for pre_page in order_rules.iter().filter(|rule| rule.get(0).unwrap() == page) {
-            if pre_printed.contains(pre_page.get(1).unwrap()) {return false;};
+        for pre_page in order_rules.iter().filter(|rule| rule.0 == *page) {
+            if pre_printed.contains(&pre_page.1) {return false;};
         }
     }
-
     true
 }
 
@@ -18,43 +17,48 @@ fn get_middle(print: &Vec<isize>) -> &isize {
     print.get(print.len() / 2).unwrap()
 }
 
-fn part1(order_rules: &Vec<Vec<isize>>, prints: &Vec<Vec<isize>>) {
-    let mut result = 0;
-    for print in prints.iter() {
-        if order(&order_rules, print) {result += get_middle(print);}
-    }
+fn part1(order_rules: &Vec<(isize, isize)>, prints: &Vec<Vec<isize>>) {
+    let result: isize = prints.iter().filter(|print| order(&order_rules, print)).map(get_middle).sum();
     println!("{result}");
 }
 
-fn force_order(order_rules: &Vec<Vec<isize>>, print: &Vec<isize>) -> Vec<isize> {
+fn topological_sort(order_rules: &Vec<(isize, isize)>, print: &Vec<isize>) -> Vec<isize> {
     let mut result = Vec::new();
 
     // edges :D
-    let mut order_rules: Vec<&Vec<isize>> = order_rules.iter().filter(|v| print.contains(v.get(0).unwrap()) && print.contains(v.get(1).unwrap())).collect();
+    let mut order_rules: Vec<&(isize, isize)> = order_rules.iter().filter(|v| print.contains(&v.0) && print.contains(&v.1)).collect();
 
     // nodes with no incoming edges
-    let mut S: Vec<&isize> = print.iter().filter(|&n| 
+    let mut free_branches: Vec<&isize> = print.iter().filter(|&n| 
             order_rules.iter().filter(|&v| 
-                v.get(1).unwrap() == n)
+                v.1 == *n)
                 .count() == 0
         ).collect();
-    while !S.is_empty() {
-        let n = *S.pop().unwrap();
+
+    while !free_branches.is_empty() {
+        let n = *free_branches.pop().unwrap();
         result.push(n);
-        order_rules = order_rules.iter().filter_map(|&v| if n != *v.get(0).unwrap() {Some(v)} else {None}).collect::<Vec<&Vec<isize>>>();
-        S.extend(print.iter().filter(|i| !result.contains(i)).filter(|&n| order_rules.iter().filter(|&v| 
-            v.get(1).unwrap() == n)
-            .count() == 0));
+
+        order_rules = order_rules.iter().filter_map(|&v| 
+                if n != v.0 {Some(v)} else {None}
+            ).collect::<Vec<&(isize, isize)>>();
+
+        free_branches.extend(print.iter()
+                .filter(|i| !result.contains(i))
+                .filter(|&n| 
+                    order_rules.iter().filter(|&v| v.1 == *n).count() == 0
+                )
+            );
     }
 
     result
 }
 
-fn part2(order_rules: &Vec<Vec<isize>>, prints: &Vec<Vec<isize>>) {
+fn part2(order_rules: &Vec<(isize, isize)>, prints: &Vec<Vec<isize>>) {
     let mut result = 0;
     for print in prints.iter() {
         if !order(order_rules, print) {
-            result += get_middle(&force_order(order_rules, print));
+            result += get_middle(&topological_sort(order_rules, print));
         }
     }
     println!("{result}");
@@ -67,9 +71,10 @@ pub fn main() {
     let order_rules = spliterator.next().unwrap();
     let prints = spliterator.next().unwrap();
 
-    let order_rules: Vec<Vec<isize>> = order_rules.split_whitespace().filter_map(|line|
+    let order_rules: Vec<(isize, isize)> = order_rules.split_whitespace().filter_map(|line|
         if !line.is_empty() {
-            Some(line.split("|").map(|v| v.parse().unwrap()).collect())
+            let mut hold = line.split("|").map(|v| v.parse::<isize>().unwrap());
+            Some((hold.next().unwrap(), hold.next().unwrap()))
         } else {None}
     ).collect();
 
