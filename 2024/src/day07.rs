5b3e68714b90dfcf0880598e6f9626e190dff949
Alviere <againes0501@gmail.com>
2024-12-06 21:23:22 -0800

    Day 7 part 2 successful

--- 2024/src/day07.rs
+++ 2024/src/day07.rs
@@ -2,17 +2,7 @@
 use std::collections::HashSet;
 use std::ops::{Add, Mul};
 
-fn part1(input: &str) {
-    let lines = input.split("\n").filter(|s| !s.is_empty()).map(|line| {
-        let mut hold = line.split(": ");
-        let goal = hold.next().unwrap().parse::<isize>().unwrap();
-        let operands = hold.next().unwrap().split_whitespace().map(|n| n.parse::<isize>().unwrap()).collect::<Vec<isize>>();
-        (
-            goal,
-            operands,
-        )
-    }).collect::<Vec<_>>();
-
+fn part1(lines: &Vec<(isize, Vec<isize>)>) {
     let mut result = 0;
     for (goal, operands) in lines {
         let mut found_numbers = HashSet::new();
@@ -27,12 +17,47 @@ fn part1(input: &str) {
             }
             found_numbers = next_found_numbers;
         }
-        if found_numbers.contains(&goal) {result += goal; println!("{goal}: {operands:?}");}
+        if found_numbers.contains(&goal) {result += goal;}
+    }
+    println!("{result}");
+}
+
+fn concatenate(left: isize, right:isize) -> isize {
+    (left.to_string() + &right.to_string()).parse::<isize>().unwrap()
+}
+
+fn part2(lines: &Vec<(isize, Vec<isize>)>) {
+    let mut result = 0;
+    for (goal, operands) in lines {
+        let mut found_numbers = HashSet::new();
+        for &operand in operands.iter() {
+            let mut next_found_numbers = HashSet::new();
+            if found_numbers.is_empty() {found_numbers.insert(operand); continue;}
+
+            for number in found_numbers {
+                for operator in vec![Add::add, Mul::mul, concatenate] {
+                    next_found_numbers.insert(operator(number, operand));
+                }
+            }
+            found_numbers = next_found_numbers;
+        }
+        if found_numbers.contains(&goal) {result += goal;}
     }
     println!("{result}");
 }
 
 pub fn main() {
     let input = crate::grab_input("day07");
+    let input = input.split("\n").filter(|s| !s.is_empty()).map(|line| {
+        let mut hold = line.split(": ");
+        let goal = hold.next().unwrap().parse::<isize>().unwrap();
+        let operands = hold.next().unwrap().split_whitespace().map(|n| n.parse::<isize>().unwrap()).collect::<Vec<isize>>();
+        (
+            goal,
+            operands,
+        )
+    }).collect::<Vec<_>>();
+
     part1(&input);
+    part2(&input);
 }
