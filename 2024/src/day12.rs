825a2188e038c60ce8f15dc258ce162f90a66641
Alviere <againes0501@gmail.com>
2024-12-11 22:15:36 -0800

    Day 12 cleanup

--- 2024/src/day12.rs
+++ 2024/src/day12.rs
@@ -2,7 +2,10 @@
 use std::collections::{HashMap, HashSet};
 use crate::{Coord, New};
 
-fn group(map: &HashMap<Coord, char>, nucleation: Coord) -> HashSet<Coord> {
+fn group<T>(map: &HashMap<Coord, T>, nucleation: Coord) -> HashSet<Coord> 
+where
+    T: PartialEq
+{
     let mut visited = HashSet::new();
     let neighbours = vec![Coord{x:1,y:0}, Coord{x:-1,y:0}, Coord{x:0,y:1}, Coord{x:0,y:-1}];
     let group_id = map.get(&nucleation);
@@ -39,38 +42,19 @@ fn part1(groups: &Vec<HashSet<Coord>>) {
 
 fn get_fencelines(patch: &HashSet<Coord>) -> usize {
     let fence_units = get_fencing(patch);
-    let neighbours = vec![Coord{x:1,y:0}, Coord{x:0,y:1}, Coord{x:-1,y:0}, Coord{x:0,y:-1}];
 
     let mut lines = Vec::<HashSet<Coord>>::new();
     for direction in 0..4usize {
         let mut fence_units = fence_units.iter()
             .filter_map(|(coord, dir)| 
-                if direction == *dir {Some(*coord)} 
+                if direction == *dir {Some((*coord, *dir))}
                 else {None}
-            ).collect::<HashSet<Coord>>();
+            ).collect::<HashMap<Coord, usize>>();
         
-        // if 1,0 or -1,0 then dir%2 == 0, if 0,1 or 0,-1 then dir%2 == 1
-        let neighbours = neighbours.iter().enumerate()
-            .filter_map(|(dir, coord)| 
-                if direction % 2 != dir % 2 {Some(*coord)} else {None})
-            .collect::<Vec<Coord>>();
-
         while !fence_units.is_empty() {
-            let start = *fence_units.iter().next().unwrap();
-            fence_units.remove(&start);
-
-            let mut visited = HashSet::<Coord>::new();
-            let mut travel_stack = vec![start];
-            while !travel_stack.is_empty() {
-                let here = travel_stack.pop().unwrap();
-                visited.insert(here);
-                for neighbour in neighbours.iter().map(|n| here + *n) {
-                    if fence_units.contains(&neighbour) {
-                        fence_units.remove(&neighbour);
-                        travel_stack.push(neighbour);
-                    }
-                }
-            }
+            let start = *fence_units.keys().next().unwrap();
+            let visited = group(&fence_units, start);
+            fence_units.retain(|c, _| !visited.contains(c));
             lines.push(visited);
         }
     }
@@ -101,7 +85,6 @@ pub fn main() {
         })
         .flat_map(std::convert::identity)
         .collect();
-
     
     let mut groups = Vec::<HashSet<Coord>>::new();
     let mut visited = HashSet::<Coord>::new();
