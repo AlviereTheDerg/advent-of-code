5403cb5d9272dbbc661e0b1890197ed569ebeedc
Alviere <againes0501@gmail.com>
2024-12-14 22:28:10 -0800

    Day 15 part 2 successful

--- 2024/src/day15.rs
+++ 2024/src/day15.rs
@@ -1,5 +1,5 @@
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use crate::{Coord, New};
 
 #[derive(Debug, Clone)]
@@ -37,38 +37,112 @@ impl StorageMap {
     pub fn grade(&self) -> isize {
         self.data.iter()
             .filter_map(|(coord, char)|
-                if *char == 'O' {
+                if *char == 'O' || *char == '[' {
                     Some(coord.x + 100*coord.y)
                 } else {None}
             )
             .sum()
     }
 
-    pub fn robot_move(&mut self, direction: Coord) {
-        let mut hold = self.robot;
+    fn can_robot_move(&self, direction: Coord) -> Option<HashSet<Coord>> {
+        let mut all_hold = HashSet::new();
+        let mut expansion_stack = vec![self.robot + direction];
         // empty space check
-        while true {
-            hold = hold + direction;
-            if self.data.get(&hold) == Some(&'#') {return;} // check hit a wall (can't move here)
-            if !hold.within_bounds(&self.bounds) {return;} // check went OOB (can't move here)
-            if !self.data.contains_key(&hold) {break;} // check found empty space (continue)
+        while !expansion_stack.is_empty() {
+            let hold = expansion_stack.pop().unwrap();
+            if all_hold.contains(&hold) {continue;}
+            all_hold.insert(hold);
+            if self.data.get(&hold) == Some(&'#') {return None;} // check hit a wall (can't move here)
+            if !hold.within_bounds(&self.bounds) {return None;} // check went OOB (can't move here)
+            if let Some(char) = self.data.get(&hold) {
+                match char {
+                    'O' => expansion_stack.push(hold + direction),
+                    '[' => {
+                        all_hold.insert(hold + Coord::new(1isize, 0isize));
+                        expansion_stack.push(hold + direction); 
+                        expansion_stack.push(hold + direction + Coord::new(1isize,0isize))
+                    },
+                    ']' => {
+                        all_hold.insert(hold + Coord::new(-1isize, 0isize));
+                        expansion_stack.push(hold + direction); 
+                        expansion_stack.push(hold + direction + Coord::new(-1isize,0isize))
+                    },
+                    _ => {},
+                }
+            }
         }
-        let destination = self.robot + direction;
-        // if the space right in front of the robot isn't open
-        if hold != destination {
-            self.data.insert(hold, *self.data.get(&destination).unwrap());
-            self.data.remove(&destination);
-        }
+        Some(all_hold)
+    }
 
-        self.robot = destination;
+    pub fn robot_move(&mut self, direction: Coord) {
+        if let Some(mut hold) = self.can_robot_move(direction) {
+            hold.retain(|coord| self.data.contains_key(coord));
+            while !hold.is_empty() {
+                let moving = hold.iter()
+                    .filter_map(|&coord|
+                        if !self.data.contains_key(&(coord + direction)) {Some(coord)} else {None}
+                    )
+                    .collect::<HashSet<Coord>>();
+                for mover in moving.iter() {
+                    let moved = self.data.remove(&mover).unwrap();
+                    self.data.insert(*mover + direction, moved);
+                }
+                hold.retain(|coord| !moving.contains(coord));
+            }
+            self.robot = self.robot + direction;
+        }
+    }
+
+    pub fn enwiden(self) -> Self {
+        let robot = Coord::new(self.robot.x * 2, self.robot.y);
+
+        let bounds = Coord::new(self.bounds.x * 2, self.bounds.y);
+        
+        let data =
+            self.data.into_iter()
+                .map(|(coord, char_d)| {
+                    let base = Coord::new(2 * coord.x, coord.y);
+                    match char_d {
+                        '#' => vec![(base, char_d), (base + Coord::new(1isize, 0isize), char_d)].into_iter(),
+                        'O' => vec![(base, '['), (base + Coord::new(1isize, 0isize), ']')].into_iter(),
+                        _ => vec![].into_iter(),
+                    }
+                })
+                .flat_map(std::convert::identity)
+                .collect::<HashMap<Coord, char>>();
+
+        Self{
+            robot,
+            bounds,
+            data,
+        }
+    }
+
+    pub fn print(&self) {
+        for y in 0..self.bounds.y {
+            let line = (0..self.bounds.x)
+                .map(|x|
+                    match self.data.get(&Coord::new(x,y)) {
+                        Some(ch) => *ch,
+                        None => {
+                            if Coord::new(x,y) == self.robot {
+                                '@'
+                            } else {'.'}
+                        }
+                    }
+                ).collect::<String>();
+            println!("{line}");
+        }
     }
 }
 
-fn part1(mut input: StorageMap, directions: &Vec<Coord>) {
+fn print_final_grade(mut input: StorageMap, directions: &Vec<Coord>) {
+    //input.print();
     for direction in directions {
         input.robot_move(*direction);
     }
     println!("{}", input.grade());
+    //input.print();
 }
 
 pub fn main() {
@@ -88,5 +162,6 @@ pub fn main() {
         )
         .collect();
 
-    part1(map.clone(), &directions);
+    print_final_grade(map.clone(), &directions);
+    print_final_grade(map.enwiden(), &directions);
 }
