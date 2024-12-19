03e36a6c559d018996215f5ed031ede187c7b89b
Alviere <againes0501@gmail.com>
2024-12-18 22:29:58 -0800

    Day 19 part 2 successful (recursive)

--- 2024/src/day19.rs
+++ 2024/src/day19.rs
@@ -1,46 +1,59 @@
 
-use std::collections::{VecDeque, HashSet};
+use std::collections::HashMap;
 
-fn can_towel_be_made(wanted_towel: &str, available_towels: &Vec<String>) -> bool {
-    let mut exploration: VecDeque<String> = VecDeque::new();
-    let mut explored: HashSet<String> = HashSet::new();
-    exploration.push_front(String::from(""));
+fn towel_permutations(wanted_towel: &str, available_towels: &Vec<&str>, memoizer: &mut HashMap<String, usize>) -> usize {
+    if wanted_towel == "" {return 1;}
 
-    while let Some(current_towel) = exploration.pop_front() {
-        if explored.contains(&current_towel) {continue;}
-        explored.insert(current_towel.clone());
-        for available_towel in available_towels.iter() {
-            let current_towel = current_towel.clone() + available_towel;
-            if current_towel == wanted_towel {return true;}
+    let mut founds = 0;
+    for &available_towel in available_towels.iter() {
+        if !wanted_towel.starts_with(available_towel) {continue;}
 
-            if current_towel.len() <= wanted_towel.len() && wanted_towel.starts_with(&current_towel) {
-                exploration.push_front(current_towel);
-            }
+        let next_towel = &wanted_towel[available_towel.len()..];
+        if let Some(amount) = memoizer.get(next_towel) {founds += amount;}
+        else {
+            let this_find = towel_permutations(next_towel, available_towels, memoizer);
+            memoizer.insert(next_towel.to_string(), this_find);
+            founds += this_find;
         }
     }
 
-    false
+    founds
 }
 
-fn part1(available_towels: &Vec<String>, wanted_towels: &Vec<String>) {
+/*
+fn part1(available_towels: &Vec<&str>, wanted_towels: &Vec<&str>) {
     let result = wanted_towels.iter()
-        .filter(|wanted_towel| can_towel_be_made(wanted_towel, available_towels))
+        .filter(|wanted_towel| towel_permutations(wanted_towel, available_towels) > 0)
         .count();
     println!("{result}");
 }
+
+fn part2(available_towels: &Vec<&str>, wanted_towels: &Vec<&str>) {
+    let result: usize = wanted_towels.iter()
+        .map(|wanted_towel| towel_permutations(wanted_towel, available_towels))
+        .sum();
+    println!("{result}");
+}
+ */
 
 pub fn main() {
     let input = crate::grab_input("day19");
     let mut spliterator = input.split("\n\n");
     let available_towels = spliterator.next().unwrap()
         .split(", ")
-        .map(str::to_string)
         .collect::<Vec<_>>();
     let wanted_towels = spliterator.next().unwrap()
         .split_whitespace()
         .filter(|s| !s.is_empty())
-        .map(str::to_string)
         .collect::<Vec<_>>();
 
-    part1(&available_towels, &wanted_towels);
+    let mut memoizer = HashMap::new();
+    let search_results = wanted_towels.iter()
+        .map(|wanted_towel| towel_permutations(wanted_towel, &available_towels, &mut memoizer))
+        .collect::<Vec<usize>>();
+
+    println!("{}", search_results.iter().filter(|&&v| v > 0usize).count());
+    println!("{}", search_results.iter().sum::<usize>());
+    //part1(&available_towels, &wanted_towels);
+    //part2(&available_towels, &wanted_towels);
 }
