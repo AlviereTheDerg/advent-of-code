3b179ae49ca5823c283304093e79c6f5b0be05ff
Alviere <againes0501@gmail.com>
2024-12-18 22:36:22 -0800

    Day 19 cleanup

--- 2024/src/day19.rs
+++ 2024/src/day19.rs
@@ -4,37 +4,21 @@ use std::collections::HashMap;
 fn towel_permutations(wanted_towel: &str, available_towels: &Vec<&str>, memoizer: &mut HashMap<String, usize>) -> usize {
     if wanted_towel == "" {return 1;}
 
-    let mut founds = 0;
-    for &available_towel in available_towels.iter() {
-        if !wanted_towel.starts_with(available_towel) {continue;}
-
-        let next_towel = &wanted_towel[available_towel.len()..];
-        if let Some(amount) = memoizer.get(next_towel) {founds += amount;}
-        else {
-            let this_find = towel_permutations(next_towel, available_towels, memoizer);
-            memoizer.insert(next_towel.to_string(), this_find);
-            founds += this_find;
-        }
-    }
-
-    founds
+    available_towels.iter()
+        .map(|&available_towel| 
+            if !wanted_towel.starts_with(available_towel) {0}
+            else {
+                let next_towel = &wanted_towel[available_towel.len()..];
+                if let Some(&amount) = memoizer.get(next_towel) {amount}
+                else {
+                    let this_find = towel_permutations(next_towel, available_towels, memoizer);
+                    memoizer.insert(next_towel.to_string(), this_find);
+                    this_find
+                }
+            }
+        )
+        .sum()
 }
-
-/*
-fn part1(available_towels: &Vec<&str>, wanted_towels: &Vec<&str>) {
-    let result = wanted_towels.iter()
-        .filter(|wanted_towel| towel_permutations(wanted_towel, available_towels) > 0)
-        .count();
-    println!("{result}");
-}
-
-fn part2(available_towels: &Vec<&str>, wanted_towels: &Vec<&str>) {
-    let result: usize = wanted_towels.iter()
-        .map(|wanted_towel| towel_permutations(wanted_towel, available_towels))
-        .sum();
-    println!("{result}");
-}
- */
 
 pub fn main() {
     let input = crate::grab_input("day19");
@@ -54,6 +38,4 @@ pub fn main() {
 
     println!("{}", search_results.iter().filter(|&&v| v > 0usize).count());
     println!("{}", search_results.iter().sum::<usize>());
-    //part1(&available_towels, &wanted_towels);
-    //part2(&available_towels, &wanted_towels);
 }
