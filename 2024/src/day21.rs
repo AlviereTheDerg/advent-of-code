b16366fd1b1ce42ba561f3f4095de46f2725e55d
Alviere <againes0501@gmail.com>
2024-12-21 16:11:24 -0800

    Day 21 part 2 successful

--- 2024/src/day21.rs
+++ 2024/src/day21.rs
@@ -1,19 +1,11 @@
 
-use std::collections::{HashMap, VecDeque};
-use itertools::Itertools;
+use std::collections::HashMap;
 
 use crate::{Coord, New};
 
 // code -> list of lists of chars, chars are buttons, 
 // each inner list is a unique shortest path s.t. all paths go from the same start to the same end
-fn get_numpad_robot_inputs(numpad: &HashMap<char, Coord>, code: &str) -> String {
-    let neighbours: HashMap<Coord, char> = vec![
-        (Coord{x:1,y:0}, '>'), 
-        (Coord{x:-1,y:0}, '<'), 
-        (Coord{x:0,y:1}, 'v'), 
-        (Coord{x:0,y:-1}, '^'),
-    ].into_iter().collect();
-
+fn get_numpad_robot_inputs(numpad: &HashMap<char, Coord>, code: &str) -> Vec<String> {
     let mut result = vec![];
     let mut current_key = 'A'; // always start on A
     for button in code.chars() {
@@ -27,26 +19,27 @@ fn get_numpad_robot_inputs(numpad: &HashMap<char, Coord>, code: &str) -> String 
         let hori_move = (0..diff.x.abs()).map(|_| if diff.x > 0 {'>'} else {'<'}).collect::<String>();
         
         // hand-found best paths
-        // if we go down and the corner crossed is present, we can go down first then horizontal
-        if diff.y > 0 && numpad.values().any(|coord| coord == &Coord::new(start_coord.x, end_coord.y)) {
-            result.push(vert_move + &hori_move + &"A");
-        }
-        // otherwise try horizontal first then vertical
-        else if numpad.values().any(|coord| coord == &Coord::new(end_coord.x, start_coord.y)) {
-            result.push(hori_move + &vert_move + &"A");
-        }
-        // have to go vertical first then horizontal
-        else {
-            result.push(vert_move + &hori_move + &"A");
-        }
-
-        current_key = numpad.iter().filter_map(|(&ch, &co)| if co == end_coord {Some(ch)} else {None}).next().unwrap();
+        result.push(
+            // if we go right but the down-first corner crossed is present, we go down first then horizontal
+            if diff.x > 0 && numpad.values().any(|coord| coord == &Coord::new(start_coord.x, end_coord.y)) {
+                vert_move + &hori_move
+            }
+            // otherwise try horizontal first then vertical
+            else if numpad.values().any(|coord| coord == &Coord::new(end_coord.x, start_coord.y)) {
+                hori_move + &vert_move
+            }
+            // have to go vertical first then horizontal
+            else {
+                vert_move + &hori_move
+            } 
+            + &"A"
+        );
+        current_key = button;
     }
-
-    result.join("")
+    result
 }
 
-fn part1(codes: &Vec<&str>) {
+fn sum_of_complexities_through_n_robots(codes: &Vec<&str>, mediator_bots: usize) {
     let numpad: HashMap<char, Coord> = vec![
         (Coord::new(2isize, 0isize), '9'),
         (Coord::new(1isize, 0isize), '8'),
@@ -71,17 +64,25 @@ fn part1(codes: &Vec<&str>) {
 
     let mut result = 0;
     for &code in codes.iter() {
-        let flag = false;//code == "379A";
-        let pressure_bot = get_numpad_robot_inputs(&numpad, code);
-        if flag {println!("{pressure_bot:?}")};
-        let radiation_bot = get_numpad_robot_inputs(&directional_pad, &pressure_bot);
-        if flag {println!("{radiation_bot}")};
-        let cold_bot = get_numpad_robot_inputs(&directional_pad, &radiation_bot);
-        if flag {println!("{cold_bot}")};
+        let mut pebble_time: HashMap<String, usize> = vec![
+            (get_numpad_robot_inputs(&numpad, code).join(""), 1)
+        ].into_iter().collect();
 
-        let final_code = cold_bot;
-        println!("{} * {}", final_code.len(), &code[..3]);
-        result += final_code.len() * &code[..3].parse::<usize>().unwrap();
+        for _ in 0..mediator_bots {
+            let mut next_pebbles: HashMap<String, usize> = HashMap::new();
+
+            for (sequence, count) in pebble_time.into_iter() {
+                for subsequence in get_numpad_robot_inputs(&directional_pad, &sequence) {
+                    next_pebbles.entry(subsequence)
+                        .and_modify(|v| *v += count)
+                        .or_insert(count);
+                }
+            }
+
+            pebble_time = next_pebbles;
+        }
+        let length = pebble_time.iter().map(|(sequence, count)| sequence.len() * count).sum::<usize>();
+        result += length * &code[..3].parse::<usize>().unwrap();
     }
     println!("{result}");
 }
@@ -89,5 +90,6 @@ fn part1(codes: &Vec<&str>) {
 pub fn main() {
     let input = crate::grab_input("day21");
     let input = input.split_whitespace().filter(|s| !s.is_empty()).collect::<Vec<_>>();
-    part1(&input);
+    sum_of_complexities_through_n_robots(&input, 2);
+    sum_of_complexities_through_n_robots(&input, 25);
 }
