386bc634f8b3f235758dc5b0aebd8e1008cbc94e
Alviere <againes0501@gmail.com>
2025-12-01 21:40:52 -0800

    Day 2 part 2 successful

--- 2025/src/day02.rs
+++ 2025/src/day02.rs
@@ -17,6 +17,32 @@ fn part1(input: &Vec<(isize, isize)>) {
     println!("{invalid_sum}");
 }
 
+fn check_validity_2(input: isize) -> bool {
+    let length = input.checked_ilog10().unwrap_or(0) + 1;
+    for prime in vec![2, 3, 5, 7, 11] {
+        if length % prime != 0 {continue}
+
+        let block_length = length / prime;
+        let base = input % 10_isize.pow(block_length);
+        let mut flag = true;
+        for block in 1..prime {
+            if base != (input / 10_isize.pow(block_length * block)) % 10_isize.pow(block_length) {flag = false; break;};
+        }
+        if flag {return false};
+    }
+    true
+}
+
+fn part2(input: &Vec<(isize, isize)>) {
+    let mut invalid_sum: isize = 0;
+    for (a,b) in input {
+        for c in *a..=*b {
+            if !check_validity_2(c) {invalid_sum += c};
+        }
+    }
+    println!("{invalid_sum}");
+}
+
 
 pub fn main() {
     let input = crate::grab_input("day02");
@@ -28,6 +54,8 @@ pub fn main() {
             (a.parse().unwrap(), b.parse().unwrap())
         })
         .collect();
+    
 
     part1(&ranges);
+    part2(&ranges);
 }
