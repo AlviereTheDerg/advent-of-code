e79686aadec4d29342079ef9b3d7e828495ed22c
Alviere <againes0501@gmail.com>
2025-12-05 21:42:30 -0800

    Day 6 part 2 successful

--- 2025/src/day06.rs
+++ 2025/src/day06.rs
@@ -1,6 +1,15 @@
 
 
-fn part1(operations: &Vec<Vec<&str>>) {
+fn part1(input: &str) {
+    let data: Vec<Vec<&str>> = input.split("\n").filter(|str| !str.is_empty())
+        .map(|line| {
+            line.split_whitespace().collect()
+        })
+        .collect();
+    let operations: Vec<Vec<&str>> = (0..data.first().unwrap().len()).map(|index| {
+            data.iter().map(|row| *row.get(index).unwrap()).collect()
+        })
+        .collect();
     let mut result: u64 = 0;
 
     for operation in operations {
@@ -19,17 +28,40 @@ fn part1(operations: &Vec<Vec<&str>>) {
     println!("{}", result);
 }
 
+fn part2(input: &str) {
+    let mut rows: Vec<_> = input.split("\n").filter(|str| !str.is_empty()).map(|row| row.chars()).collect();
+    let columns: Vec<_> = (0..input.split_once("\n").unwrap().0.len())
+        .map(|_| {
+            rows.iter_mut().map(|row| row.next().unwrap()).collect::<String>()
+        })
+        .collect();
+    let blocks: Vec<_> = columns.split(|column| column.trim().is_empty()).collect();
+
+    let mut result: u64 = 0;
+
+    for block in blocks {
+        let op = block[0].chars().last().unwrap();
+        let mut acc: u64 = if op == '*' {1} else {0};
+
+        for line in block {
+            let stripped_line = line.chars().filter(|ch| "0123456789".contains(*ch)).collect::<String>();
+            let val: u64 = stripped_line.parse().unwrap();
+            if op == '*' {
+                acc *= val;
+            } else {
+                acc += val;
+            }
+        }
+
+        result += acc;
+    }
+
+    println!("{}", result);
+}
+
 pub fn main() {
     let input = crate::grab_input("day06");
-    let data: Vec<Vec<&str>> = input.split("\n").filter(|str| !str.is_empty())
-        .map(|line| {
-            line.split_whitespace().collect()
-        })
-        .collect();
-    let flipways: Vec<Vec<&str>> = (0..data.first().unwrap().len()).map(|index| {
-            data.iter().map(|row| *row.get(index).unwrap()).collect()
-        })
-        .collect();
 
-    part1(&flipways);
+    part1(&input);
+    part2(&input);
 }
