f8ea0e29d3b5cf61ac4176151f3d75d2f76273f9
Alviere <againes0501@gmail.com>
2025-12-06 21:47:27 -0800

    Day 7 cleanup

--- 2025/src/day07.rs
+++ 2025/src/day07.rs
@@ -2,43 +2,21 @@
 use std::collections::{HashMap, HashSet};
 use crate::{Coord, New};
 
-fn part1(charsmap: &HashMap<Coord, char>, rows: usize, start: Coord, columns: usize) {
-    let mut charsmap = charsmap.clone();
-    let mut splitters_hit: HashSet<Coord> = HashSet::new();
-    let mut beam_heads: HashSet<Coord> = HashSet::from([start]);
+pub fn main() {
+    let input = crate::grab_input("day07");
+    let mut charsmap: HashMap<Coord, char> = input.split_whitespace().enumerate()
+        .flat_map(|(row_index, row)| {
+            row.chars().enumerate().map(move |(column_index, ch)| {
+                (Coord::new(column_index, row_index), ch)
+            })
+        })
+        .filter(|(_, ch)| *ch != '.')
+        .collect();
 
-    let beam_propagation = Coord{x:0, y:1};
-    let splitter_propagation = vec![Coord{x:-1, y:0}, Coord{x:1, y:0}];
+    let rows = input.split_whitespace().count();
+    let columns = input.split_whitespace().next().unwrap().len();
+    let start = *charsmap.iter().filter_map(|(coord, ch)| if *ch == 'S' {Some(coord)} else {None}).next().unwrap();
 
-    loop {
-        let mut new_beam_heads: HashSet<Coord> = HashSet::new();
-
-        for beam in beam_heads {
-            if beam.y > rows as isize {continue;}
-            if let Some(hit) = charsmap.get(&beam) && hit == &'^' {
-                splitters_hit.insert(beam);
-                new_beam_heads.extend(splitter_propagation.iter().map(|&coord| coord + beam));
-            } else {
-                new_beam_heads.insert(beam + beam_propagation);
-                charsmap.insert(beam, '|');
-            }
-        }
-        
-        if new_beam_heads.len() == 0 {break;}
-        beam_heads = new_beam_heads;
-    }
-
-    println!("{}", splitters_hit.len());
-    for y in 0..rows {
-        for x in 0..columns {
-            print!("{}", charsmap.get(&Coord::new(x,y)).unwrap_or(&'.'));
-        }
-        println!("");
-    }
-}
-
-fn part2(charsmap: &HashMap<Coord, char>, rows: usize, start: Coord, columns: usize) {
-    let mut charsmap = charsmap.clone();
     let mut splitters_hit: HashSet<Coord> = HashSet::new();
     let mut beam_heads: HashMap<Coord, usize> = HashMap::from([(start, 1usize)]);
     let mut retired_beam_heads: HashMap<Coord, usize> = HashMap::new();
@@ -68,31 +46,16 @@ fn part2(charsmap: &HashMap<Coord, char>, rows: usize, start: Coord, columns: us
         if new_beam_heads.len() == 0 {break;}
         beam_heads = new_beam_heads;
     }
-
-    println!("{}", retired_beam_heads.values().sum::<usize>());
+    
+    // beam printing!
     for y in 0..rows {
         for x in 0..columns {
             print!("{}", charsmap.get(&Coord::new(x,y)).unwrap_or(&'.'));
         }
         println!("");
     }
-}
 
-pub fn main() {
-    let input = crate::grab_input("day07");
-    let charsmap: HashMap<Coord, char> = input.split_whitespace().enumerate()
-        .flat_map(|(row_index, row)| {
-            row.chars().enumerate().map(move |(column_index, ch)| {
-                (Coord::new(column_index, row_index), ch)
-            })
-        })
-        .filter(|(_, ch)| *ch != '.')
-        .collect();
-
-    let rows = input.split_whitespace().count();
-    let columns = input.split_whitespace().next().unwrap().len();
-    let start = *charsmap.iter().filter_map(|(coord, ch)| if *ch == 'S' {Some(coord)} else {None}).next().unwrap();
-
-    part1(&charsmap, rows, start, columns);
-    part2(&charsmap, rows, start, columns);
+    // actual part 1/2 display
+    println!("{}", splitters_hit.len());
+    println!("{}", retired_beam_heads.values().sum::<usize>());
 }
