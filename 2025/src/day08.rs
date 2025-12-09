7e054b31d4948160017245525be59c4d489af822
Alviere <againes0501@gmail.com>
2025-12-08 17:48:14 -0800

    Day 8 optimization (4x speedup)

--- 2025/src/day08.rs
+++ 2025/src/day08.rs
@@ -1,31 +1,34 @@
 
-use std::collections::HashMap;
+use std::collections::BinaryHeap;
+use std::cmp::Reverse;
 
-fn dist(a: (i64,i64,i64), b:(i64,i64,i64)) -> i64 {
+fn dist(boxes: &Vec<(i64,i64,i64)>, a: usize, b: usize) -> i64 {
+    let a = boxes.get(a).unwrap();
+    let b = boxes.get(b).unwrap();
     (a.0 - b.0).pow(2) + (a.1 - b.1).pow(2) + (a.2 - b.2).pow(2)
 }
 
-fn find(parents: &mut HashMap<(i64,i64,i64),(i64,i64,i64)>, node: (i64,i64,i64)) -> (i64,i64,i64) {
-    let this_parent = *parents.get(&node).unwrap();
+fn find(parents: &mut Vec<usize>, node: usize) -> usize {
+    let this_parent = *parents.get(node).unwrap();
     if this_parent != node {
         let new_parent = find(parents, this_parent);
-        parents.insert(node, new_parent);
+        parents[node] = new_parent;
         return new_parent;
     } else {
         node
     }
 }
 
-fn union(parents: &mut HashMap<(i64,i64,i64),(i64,i64,i64)>, sizes: &mut HashMap<(i64,i64,i64), i64>, a: (i64,i64,i64), b:(i64,i64,i64)) -> i64 {
+fn union(parents: &mut Vec<usize>, sizes: &mut Vec<i64>, a: usize, b:usize) -> i64 {
     let a = find(parents, a);
     let b = find(parents ,b);
     if a != b {
-        parents.insert(a, b); // a -> b (b is new root)
-        let child_size = *sizes.get(&a).unwrap();
-        *sizes.get_mut(&b).unwrap() += child_size; // add a's size to b
+        parents[a] = b; // a -> b (b is new root)
+        let child_size = *sizes.get(a).unwrap();
+        *sizes.get_mut(b).unwrap() += child_size; // add a's size to b
     }
     // if b was not originally root, b is now root
-    *sizes.get(&b).unwrap()
+    *sizes.get(b).unwrap()
 }
 
 pub fn main() {
@@ -37,38 +40,33 @@ pub fn main() {
         })
         .collect();
 
-    let mut box_pairs: Vec<(i64, (i64,i64,i64), (i64,i64,i64))> = Vec::new();
-    let mut scanned_boxes: HashMap<(i64,i64,i64), (i64,i64,i64)> = HashMap::new();
-    let mut sizes: HashMap<(i64,i64,i64), i64> = HashMap::new();
-    for &junction in boxes.iter() {
-        for &other_junction in scanned_boxes.keys() {
-            box_pairs.push((dist(junction, other_junction), junction, other_junction));
+    let mut heap = BinaryHeap::new(); // min-heap storing (edge^2, id1, id2)
+    for junction in 0..boxes.len() {
+        for other_junction in 0..junction {
+            heap.push(Reverse(
+                (dist(&boxes, junction, other_junction), junction, other_junction)
+            ));
         }
-        scanned_boxes.insert(junction, junction);
-        sizes.insert(junction, 1);
     }
-
-    box_pairs.sort();
-    let mut box_pairs_iter = box_pairs.iter().map(|(_, a, b)| (a,b));
+    let mut parents: Vec<usize> = (0..boxes.len()).map(|v| v).collect(); // this.id -> parent.id
+    let mut sizes: Vec<i64> = (0..boxes.len()).map(|_| 1).collect(); // this.id -> this.size
+    
     for _ in 0..1000 {
-        let (&a, &b) = box_pairs_iter.next().unwrap();
-        union(&mut scanned_boxes, &mut sizes, a, b);
+        let (_, a, b) = heap.pop().unwrap().0;
+        union(&mut parents, &mut sizes, a, b);
     }
-
-    let mut circuit_sizes: HashMap<(i64,i64,i64), i64> = HashMap::new();
-    for &junction in boxes.iter() {
-        *circuit_sizes.entry(find(&mut scanned_boxes, junction)).or_insert(0) += 1;
-    }
-
-    let mut circuit_sizes_vec: Vec<_> = circuit_sizes.values().collect();
-    circuit_sizes_vec.sort(); circuit_sizes_vec.reverse();
-    let result = *circuit_sizes_vec.get(0).unwrap() * *circuit_sizes_vec.get(1).unwrap() * *circuit_sizes_vec.get(2).unwrap();
+    
+    let mut circuit_sizes_vec: Vec<_> = sizes.clone();
+    circuit_sizes_vec.sort();
+    let result: i64 = circuit_sizes_vec[circuit_sizes_vec.len() - 3..].iter().product();
     println!("{}", result); // part 1
-
+    
     let goal = boxes.len() as i64;
     loop {
-        let (&a, &b) = box_pairs_iter.next().unwrap();
-        if union(&mut scanned_boxes, &mut sizes, a, b) == goal {
+        let (_, a, b) = heap.pop().unwrap().0;
+        if union(&mut parents, &mut sizes, a, b) == goal {
+            let a = boxes.get(a).unwrap();
+            let b = boxes.get(b).unwrap();
             println!("{}", a.0 * b.0);
             break;
         }
